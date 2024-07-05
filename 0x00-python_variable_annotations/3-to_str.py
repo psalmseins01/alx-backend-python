a314fa96e4e554a324b6e3b196c343347aa644bf
Anthony Gbobo <psalmseins@gmail.com>
2024-07-04 19:37:10 -0700

    Returns the string representation of n

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Type-annotated function to_str that takes a float n
+    and returns the string representation of n
+"""
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of n """
+    return str(n)
